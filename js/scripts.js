c7a22aa68435c34b6bf87ee3b4e5fa3238086c8e
kharris27 <46464283+kharris27@users.noreply.github.com>
2020-02-26 12:58:41 -0800

    Added code from codepen

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,72 @@
 console.log('OK');
+
+function leftArrowPressed() {
+    let element = document.getElementById("Spaceship");
+    element.style.left = parseInt(element.style.left) - 5 + 'px';
+  }
+
+  function rightArrowPressed() {
+    let element = document.getElementById("Spaceship");
+    element.style.left = parseInt(element.style.left) + 5 + 'px';
+  }
+
+  function upArrowPressed() {
+    let element = document.getElementById("Spaceship");
+    element.style.top = parseInt(element.style.top) - 5 + 'px';
+  }
+
+  function downArrowPressed() {
+    let element = document.getElementById("Spaceship");
+    element.style.top = parseInt(element.style.top) + 5 + 'px';
+  }
+
+  function moveSelection(evt) {
+    switch (evt.keyCode) {
+      case 37:
+      leftArrowPressed();
+      break;
+      case 39:
+      rightArrowPressed();
+      break;
+      case 38:
+      upArrowPressed();
+      break;
+      case 40:
+      downArrowPressed();
+      break;
+    }
+  };
+
+    function docReady()
+    {
+          
+    window.addEventListener('keydown', moveSelection);
+    }
+
+gsap.from( '.box', {
+  x: 0,
+  y: 250,
+  repeat: -1,
+  repeatDelay: 1,
+  duration: 2,
+});
+
+gsap.to( '.box', {
+  x: 0,
+  y: 20,
+  repeat: -1,
+  repeatDelay: 1,  
+  duration: 2,
+});
+
+var tween = gsap.to( '#Alien', {
+  x: 350,
+  y: 0,
+  repeatDelay: 1,
+  repeat: -1, 
+  yoyo: true, 
+  duration: 3,
+  paused: true,
+});
+
+document.getElementById("play").onclick = () => tween.play();
